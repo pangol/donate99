3b048d2f36ef9ff7f10812a23c7a969b54df9b7c
Park Yong <pangol75@gmail.com>
2021-10-12 15:58:52 +0900

    phone load

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 
 import * as THREE from '../lib/three.module.js'
 import { OrbitControls } from '../lib/OrbitControls.js'
+import { GLTFLoader } from '../lib/GLTFLoader.js'
+
+/**
+ * Materials
+ */
+// Baked material
+const bakedMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const gltfLoader = new GLTFLoader()
 
 //Debug UI
 const gui = new dat.GUI()
@@ -12,18 +20,18 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 //object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
 
-gui
-    .add(mesh.position, 'y')
-    .min(- 3)
-    .max(3)
-    .step(0.01)
-    .name('elevation')
+// gui
+//     .add(mesh.position, 'y')
+//     .min(- 3)
+//     .max(3)
+//     .step(0.01)
+//     .name('elevation')
 
-scene.add(mesh)
+// scene.add(mesh)
 
 //camera
 const sizes = {
@@ -32,9 +40,49 @@ const sizes = {
 }
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 5
+camera.position.x = 3
+camera.position.y = 3
+
 scene.add(camera)
 
+gltfLoader.load(
+    '../model/iphone.glb',
+    (gltf) => {
+        gltf.scene.traverse((child) => {
+            child.material = bakedMaterial
+        })
+        scene.add(gltf.scene)
+    }
+)
+
+//Lights
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+scene.add(ambientLight)
+
+// const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+// directionalLight.position.set(0, 0, 0)
+// scene.add(directionalLight)
+
+// const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+// scene.add(hemisphereLight)
+
+// const pointLight = new THREE.PointLight(0xff9000, 0.5)
+// scene.add(pointLight)
+
+// const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+// spotLight.position.set(0, 2, 3)
+// scene.add(spotLight)
+
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+// scene.add(hemisphereLightHelper)
+
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+// scene.add(directionalLightHelper)
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+// scene.add(pointLightHelper)
 
 //controls
 const controls = new OrbitControls(camera, canvas)
@@ -61,7 +109,7 @@ const clock = new THREE.Clock()
 const tick = () => {
 
     const elapsedTime = clock.getElapsedTime()
-    mesh.rotation.y = elapsedTime
+    // mesh.rotation.y = elapsedTime
 
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
